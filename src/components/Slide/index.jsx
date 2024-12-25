96cbbcb5a3fa73a8d0553ec0b2bb7e68cccda6ed
ivanizidoro <ivanizidoro@hotmail.com>
2024-12-25 13:27:52 -0300

    feito-alteração-no-slide

--- src/components/Slide/index.jsx
+++ src/components/Slide/index.jsx
@@ -5,11 +5,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow } from "swiper/modules";
 
 const data = [
-  { id: 1, image: "/src/assets/FRENTE--LOJA.jpg" },
+  { id: 1, image: "/FRENTE--LOJA.jpg" },
   { id: 2, image: "https://www.designi.com.br/images/preview/10280847.jpg" },
   { id: 3, image: "https://www.designi.com.br/images/preview/10045449.jpg" },
   { id: 4, image: "https://www.designi.com.br/images/preview/10028403.jpg" },
-  { id: 5, image: "https://img.freepik.com/psd-premium/template-banner-horizontal-internet-500-mega-ofertas-no-brasil_379939-79.jpg" },
+  {
+    id: 5,
+    image:
+      "https://img.freepik.com/psd-premium/template-banner-horizontal-internet-500-mega-ofertas-no-brasil_379939-79.jpg",
+  },
 ];
 
 export function Slide() {
