6159fa771bf2c3c60ff44406f4fed773a39f336c
Ivan Izidoro <109913632+ivanizidoro@users.noreply.github.com>
2025-01-20 16:48:26 -0300

    Retirado o plano de 30mb da container PLANOS

--- src/container/Planos/index.jsx
+++ src/container/Planos/index.jsx
@@ -4,12 +4,11 @@ import { Header } from "../Header";
 import { Container, ContainerPlanos, H2 } from "./styles";
 
 const planos = [
-  { id: 1, image: "/planos/30.png" },
-  { id: 2, image: "/planos/100.png" },
-  { id: 3, image: "/planos/200.png" },
-  { id: 4, image: "/planos/300.png" },
-  { id: 5, image: "/planos/400.png" },
-  { id: 6, image: "/planos/600.png" },
+  { id: 1, image: "/planos/100.png" },
+  { id: 2, image: "/planos/200.png" },
+  { id: 3, image: "/planos/300.png" },
+  { id: 4, image: "/planos/400.png" },
+  { id: 5, image: "/planos/600.png" },
 ];
 
 export function Planos() {
