fd82c9764eda992df4de29e2c58bd6253926530f
ivanizidoro <ivanizidoro.com.br>
2025-02-23 19:24:52 -0300

    feito-alterações-na-aba-quemmsomos

--- src/container/Somos/styles.js
+++ src/container/Somos/styles.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
   height: 100vh;
-  
+
+  @media (max-width: 768px) {
+    margin-bottom: 80px;
+  }
 `;
 
 export const ContainerDiv = styled.div`
