c9211c166089cb4039558585fe7ad6fe682e4dc5
ivanizidoro <ivanizidoro.com.br>
2025-03-28 07:50:33 -0300

    feito-alteração-no-slider-add-foto

--- src/components/Slide/styles.js
+++ src/components/Slide/styles.js
@@ -2,16 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding-top: 100px;
+  margin: 0 auto;
+
+  max-width: 1200px;
 `;
 
 export const ContianerSlide = styled.div`
   .slider-item {
     width: 100%;
-    height: 800px;
+    height: 100%;
     object-fit: cover;
 
     @media (max-width: 768px) {
-      height: 250px;
+      height: 100%;
     }
   }
 `;
